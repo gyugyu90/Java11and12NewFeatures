5ef5c22c50e9c37d2c8005b830e4f3c52c87b565
gyugyu90 <kh2000park@gmail.com>
2021-01-21 22:24:15 +0900

    reassigning values to inferred variables

--- src/chap1/TypeInference.java
+++ src/chap1/TypeInference.java
@@ -7,5 +7,11 @@ public class TypeInference {
         var name = "Java Everywhere"; // inferred as String
         var dateTime = LocalDateTime.now(); // inferred as LocalDateTime
 
+        var age = 9;
+        // age = 10.9; // can't assign 10.9 to variable of type int
+
+        var query = new StringBuilder("SELECT"); // infer type as StringBuilder
+        // query = query.toString() + "FROM" + "TABLE"; // won't compile
+
     }
 }
