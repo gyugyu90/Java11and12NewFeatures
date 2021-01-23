5e2dd5669f5d5ebf69d29e8a9e62c1ae895e3c54
gyugyu90 <kh2000park@gmail.com>
2021-01-23 20:36:17 +0900

    comparing type inference with legacy java

--- src/chap1/TypeInference.java
+++ src/chap1/TypeInference.java
@@ -1,6 +1,10 @@
 package chap1;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class TypeInference {
     public static void main(String[] args) {
@@ -19,5 +23,13 @@ public class TypeInference {
         var letter = (char) 97;
         var debit = (float) 17.9;
 
+        List<Integer> java5ListOfIntegers = Collections.<Integer>emptyList();
+        List<Integer> modernJavaListOfIntegers = Collections.emptyList();
+
+        List<String> java7Generic = new ArrayList<String>();
+        List<String> modernJavaGeneric = new ArrayList<>();
+
+        Consumer<String> java8Consumer = (String s) -> System.out.println(s);
+        Consumer<String> modernJavaConsumer = s -> System.out.println(s);
     }
 }
