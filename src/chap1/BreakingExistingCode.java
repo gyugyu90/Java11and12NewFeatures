0b6b74651028ea2551cdda6b845ded4797b88670
gyugyu90 <kh2000park@gmail.com>
2021-01-25 22:48:10 +0900

    breaking existing code

---
+++ src/chap1/BreakingExistingCode.java
@@ -0,0 +1,25 @@
+package chap1;
+
+public class BreakingExistingCode {
+    // the following code will not compile
+
+    // class var { }
+    // interface var { }
+
+    /*
+    class Demo {
+        int var = 100;
+
+        static long var = 121;
+
+        void var() {
+            int var = 10;
+        }
+
+        void aMethod(String var) {
+
+        }
+    }
+    */
+
+}
