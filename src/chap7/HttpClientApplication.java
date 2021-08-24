d75f2ed6747937964bad49e21dd8215d0212b5be
gyugyu90 <kh2000park@gmail.com>
2021-08-24 22:10:38 +0900

    http client api

---
+++ src/chap7/HttpClientApplication.java
@@ -0,0 +1,29 @@
+package chap7;
+
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HttpClientApplication {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                                         .uri(URI.create("http://www.google.com"))
+                                         .build();
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+        HttpClient httpClient = HttpClient.newBuilder()
+                                          .version(HttpClient.Version.HTTP_2) // version
+                                          .followRedirects(HttpClient.Redirect.NORMAL) // redirect policy
+                                          .authenticator(Authenticator.getDefault())
+                                          .build();
+
+    }
+
+
+}
